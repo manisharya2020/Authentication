bf9b4d5bd702c4027042d9d71cfec989303eba29
Manish <aryam5078@gmail.com>
2022-07-21 14:17:55 +0530

    add bicrypt authentication

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-var md5 = require('md5');
+const bcrypt = require('bcrypt');
+const saltrounds = 10;
 
 const app = express();
 
@@ -39,18 +40,20 @@ app.route("/login")
   })
   .post(function(req, res) {
     const username = req.body.username;
-    const password = md5(req.body.password);
+    const password = req.body.password;
     User.findOne({
       email: username
     }, function(err, user) {
       if (err) {
         console.log(err);
       } else {
-        if (user.password === password) {
-          res.render("secrets");
-        } else {
+        bcrypt.compare(req.body.password, user.password, function(err, result) {
+          if (result == true) {
+            res.render("secrets");
+          } else {
             res.send('Did not find any account with this username.');
-        }
+          }
+        });
       }
     })
   });
@@ -62,18 +65,24 @@ app.route("/register")
     res.render("register");
   })
   .post(function(req, res) {
-    const newUser = new User({
-      email: req.body.username,
-      password: md5(req.body.password)
-    });
-    newUser.save(function(err) {
-      if (err) {
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+      if(err){
         console.log(err);
-      } else {
-        res.render("secrets");
       }
-    })
+      const newUser = new User({
+        email: req.body.username,
+        password: hash
+      });
+      newUser.save(function(err) {
+        if (err) {
+          console.log(err);
+        } else {
+          res.render("secrets");
+        }
+      })
+    });
   });
+
 // **************
 
 // Port
